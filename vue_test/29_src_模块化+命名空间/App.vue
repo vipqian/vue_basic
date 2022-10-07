38fd01e3ee017ab502440abb52cd70a501b2ebdc
qianvipfei <qianvipfei@163.com>
2022-10-08 00:01:37 +0800

    Vuex

---
+++ vue_test/29_src_模块化+命名空间/App.vue
@@ -0,0 +1,48 @@
+<!--
+ * @Author: wangyunfei
+ * @Date: 2022-09-17 19:56:04
+ * @LastEditors: wangyunfei
+ * @LastEditTime: 2022-10-07 22:44:29
+ * @Description: file content
+ * @FilePath: /vue_test/src/App.vue
+-->
+<template>
+  <div class="container" >
+    <Count></Count>
+    <Person></Person>
+  </div>
+</template>
+
+<script>
+
+
+import Count from './components/Count.vue';
+import Person from './components/Person.vue';
+
+export default {
+    name: "App",
+    components: { 
+      Count, 
+      Person
+    },
+    
+}
+</script>
+
+<style scoped>
+  .container, .foot  {
+    display: flex;
+    justify-content: space-around;
+  }
+
+  img {
+    width: 100%;
+  }
+  video {
+    width: 100%;
+  }
+  h4 {
+    text-align: center;
+  }
+  
+</style>
---
+++ vue_test/29_src_模块化+命名空间/components/Count.vue
@@ -0,0 +1,64 @@
+<!--
+ * @Author: wangyunfei
+ * @Date: 2022-10-04 20:32:59
+ * @LastEditors: wangyunfei
+ * @LastEditTime: 2022-10-07 22:42:42
+ * @Description: file content
+ * @FilePath: /vue_test/src/components/Count.vue
+-->
+<template>
+    <div>
+      <h3>和是{{sum}}</h3>
+      <h3>和放大10倍的结果是{{bigSum}}</h3>
+      <h3>我在{{school}},学习{{subject}}</h3>
+      <h3 style="color: red">学生的总人数是{{persons.length}}</h3>
+      <div>
+        <select v-model.number="n">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+        <button @click="increment(n)">+</button>
+        <button @click="decrement(n)">-</button>
+        <!-- <button @click="INCREMENT(n)">+</button>
+        <button @click="DECREMENT(n)">-</button> -->
+        <button @click="incrementOdd(n)">奇数时加</button>
+        <button @click="incrementWait(n)">等待一会再加</button>
+      </div>
+    </div>
+  
+</template>
+
+<script>
+  import { mapState, mapGetters, mapMutations, mapActions } from "vuex";
+  export default {
+    name: 'Count',
+    data() {
+      return {
+        n: 1
+      }
+    },
+    methods: {
+      ...mapMutations('CountOptions', {increment: 'INCREMENT', decrement: 'DECREMENT'}),
+      ...mapActions('CountOptions', ['incrementOdd', 'incrementWait'])
+    },
+    computed: {
+      ...mapState('CountOptions', ['sum', 'school', 'subject']),
+      ...mapState('PersonOptions', ['persons']),
+      ...mapGetters('CountOptions', ['bigSum'])
+    },
+
+  }
+</script>
+
+<style>
+  button {
+    margin-left: 5px;
+  }
+
+  h3 {
+    text-align: center;
+    background-color: burlywood;
+  }
+
+</style>
